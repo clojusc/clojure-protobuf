ab8ee723e2a8e26fe1a392c2f7e37ec9bf5eadd2
Paul Ingles <paul@oobaloo.co.uk>
2012-01-18 16:25:48 +0000

    capture result of calling protoc- print error messages if the compiler fails

--- src/leiningen/protobuf.clj
+++ src/leiningen/protobuf.clj
@@ -122,16 +122,19 @@
          (.mkdirs dest)
          (.mkdir (io/file target "proto"))
          (doseq [proto protos]
-           (println "Compiling" proto "to" dest-path)
+           (print "Compiling" proto "to" dest-path "... ")
            (extract-dependencies (io/file proto-path proto) target)
-           (sh "protoc"
-               proto
-               (str "--java_out=" dest-path)
-               "-I."
-               (str "-I" target "/proto")
-               :dir proto-path))
-         (binding [*compile?* false]
-           (javac (assoc project :java-source-path dest-path)))))))
+           (let [protoc-result (sh "protoc"
+                                   proto
+                                   (str "--java_out=" dest-path)
+                                   "-I."
+                                   (str "-I" target "/proto")
+                                   :dir proto-path)]
+             (if (= (:exit protoc-result) 0)
+               (println "Success")
+               (do (println "Error")
+                   (println (:err protoc-result))))))
+         (javac (assoc project :java-source-path dest-path))))))
 
 (defn compile-google-protobuf
   "Compile com.google.protobuf.*"
