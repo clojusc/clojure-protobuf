d1ee1406ebb74e0b70a6003a55998d47ad431289
Duncan McGreggor <oubiwann@gmail.com>
2018-07-12 15:57:03 -0500

    Added unit tests for new constrcutors.

--- test/protobuf/core_test.clj
+++ test/protobuf/core_test.clj
@@ -3,13 +3,43 @@
     [clojure.test :refer :all]
     [protobuf.core :as protobuf])
   (:import
-    (protobuf.examples.tutorial AddressBookProtos$AddressBook)))
+    (com.google.protobuf CodedInputStream)
+    (java.io ByteArrayInputStream)
+    (protobuf.examples.tutorial AddressBookProtos$Person$PhoneNumber)))
 
-(deftest ->schema
-  (let [AddressBook (protobuf/create AddressBookProtos$AddressBook)
-        schema (protobuf/->schema AddressBook)]
-    (is (= :home
-           (get-in (protobuf/->schema AddressBook)
-                   [:fields :people :values
-                    :fields :phones :values
-                    :fields :type :default])))))
+(deftest create-flatland
+  (let [data {:number "555-1212" :type :home}
+        sample-phone-number (protobuf/create :flatland
+                             AddressBookProtos$Person$PhoneNumber
+                             data)]
+    (testing "map-based constructor ..."
+      (is (= protobuf.impl.flatland.core.FlatlandProtoBuf
+             (type sample-phone-number)))
+      (is (= data
+             (into {} sample-phone-number))))
+    (testing "bytes-based constructor ..."
+      (let [from-bytes (protobuf/create :flatland
+                        AddressBookProtos$Person$PhoneNumber
+                        (protobuf/->bytes sample-phone-number))]
+        (is (= protobuf.impl.flatland.core.FlatlandProtoBuf
+               (type from-bytes)))
+        (is (= data
+               (into {} from-bytes)))))
+    (testing "Google CodedInputStream-based constructor ..."
+      (let [from-stream (protobuf/create :flatland
+                         AddressBookProtos$Person$PhoneNumber
+                         (CodedInputStream/newInstance
+                         (protobuf/->bytes sample-phone-number)))]
+        (is (= protobuf.impl.flatland.core.FlatlandProtoBuf
+               (type from-stream)))
+        (is (= data
+               (into {} from-stream)))))
+    (testing "Java InputStream-based constructor ..."
+      (let [from-stream (protobuf/create :flatland
+                         AddressBookProtos$Person$PhoneNumber
+                         (new ByteArrayInputStream
+                              (protobuf/->bytes sample-phone-number)))]
+        (is (= protobuf.impl.flatland.core.FlatlandProtoBuf
+               (type from-stream)))
+        (is (= data
+               (into {} from-stream)))))))
